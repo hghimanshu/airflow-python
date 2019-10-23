7276e7bef9bc00ee9b4be392d11f20723be6226f
hghimanshu <hghimanshu81@gmail.com>
2019-10-23 11:55:56 +0530

    added args for arguments

--- dags/inferenceReqDAG.py
+++ dags/inferenceReqDAG.py
@@ -3,8 +3,8 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
 from file_writing import filewriting
 
-def checkCond(i):
-    if int(i) %2 == 0:
+def checkCond(*args):
+    if int(*args) %2 == 0:
         return 'writing_files'
 
 
@@ -13,6 +13,8 @@ dag = DAG('hello_world', description='DAG for writing txt files',
           start_date=datetime(2019, 10, 23), catchup=False)
 
 for i in range(20):
-    branch_op = BranchPythonOperator(task_id='branch_task', provide_context=True, python_callable=checkCond(i), dag=dag)
-    file_operator = PythonOperator(task_id='writing_files', python_callable=filewriting(i), dag=dag)
+    branch_op = BranchPythonOperator(task_id='branch_task', provide_context=True, python_callable=checkCond, 
+                                        op_args=i, dag=dag)
+    file_operator = PythonOperator(task_id='writing_files', python_callable=filewriting, 
+                                        op_args=i, dag=dag)
     branch_op >> file_operator
